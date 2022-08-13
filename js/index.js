83c665f477879607d73e095d46b932adfc4a75fb
NicicolasArfel <nicolasarfel@gmail.com>
2022-08-13 09:36:45 +0200

    box-shadow for the navbar

--- js/index.js
+++ js/index.js
@@ -5,21 +5,16 @@ const app = {
 
   toggleBurger: () => {
     const burger = document.querySelector('.navbar-burger');
-    const main = document.querySelector('.main');
-    const navbarItem = document.querySelector('.navbar-item');
 
     burger.addEventListener('click', () => {
       const navbar = document.querySelector('.navbar-menu');
       if (navbar.classList.contains('is-active')) {
         navbar.classList.remove('is-active');
         burger.classList.remove('is-active');
-        main.style.opacity = '1';
-        navbarItem.style.opacity = '1';
       } else {
         burger.classList.add('is-active');
         navbar.classList.add('is-active');
-        main.style.opacity = '0.2';
-        navbarItem.style.opacity = '0.2';
+        navbar.style.boxShadow = '-50px 0px 0px 80px rgba(0,0,0,0.5)';
       }
     });
   },
